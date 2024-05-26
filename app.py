a47afeb54e891f24d105a662ef04f02c7363ae20
Tejesh Itha <39544489+ivsntejesh@users.noreply.github.com>
2024-05-26 20:09:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,7 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 def get_pdf_text(pdf_docs):
     text=""
